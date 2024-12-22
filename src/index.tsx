8ba124eefd756e9db24545b45f7e2f14ce7a3087
Zaur Yusufov <zaury8252@gmail.com>
2024-12-23 00:44:56 +0400

    commit for GitHub Pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import App from './App';
 import './index.css';
 
@@ -8,11 +8,9 @@ const container = document.getElementById('root');
 if (container) {
     const root = createRoot(container);
     root.render(
-        <React.StrictMode>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </React.StrictMode>
+        <HashRouter>
+            <App />
+        </HashRouter>
     );
 } else {
     console.error('No root element found');
